2f2be5fd055ac062fd60c8fece9973a75c665b23
Chris Joakim <christopher.joakim@gmail.com>
2024-09-27 07:51:02 -0400

    sample-program.py now processing all of the sample tcx and gpx files

--- sample-program.py
+++ sample-program.py
@@ -23,14 +23,38 @@ if __name__ == "__main__":
 
     print("ggps version {}".format(ggps.VERSION))
 
+    # Latest files produced in 2024 with Forerunner 265S
+    parse_file("data/activity_17075053124_lorimer_avinger.tcx", "tcx")
+    parse_file("data/activity_17075053124_lorimer_avinger.tcx", "path")
+    parse_file("data/activity_17075053124_lorimer_avinger.gpx", "gpx")
+    parse_file("data/activity_17075053124_lorimer_avinger.gpx", "path")
+
     # Twin Cities Marathon files
     parse_file("data/twin_cities_marathon.tcx", "tcx")
     parse_file("data/twin_cities_marathon.tcx", "path")
     parse_file("data/twin_cities_marathon.gpx", "gpx")
     parse_file("data/twin_cities_marathon.gpx", "path")
 
-    # Latest files produced in 2024 with Forerunner 265S
-    parse_file("data/activity_17075053124_lorimer_avinger.tcx", "tcx")
-    parse_file("data/activity_17075053124_lorimer_avinger.tcx", "path")
-    parse_file("data/activity_17075053124_lorimer_avinger.gpx", "gpx")
-    parse_file("data/activity_17075053124_lorimer_avinger.gpx", "path")
+    # New River 50K files
+    parse_file("data/new_river_50k.tcx", "tcx")
+    parse_file("data/new_river_50k.tcx", "path")
+    parse_file("data/new_river_50k.gpx", "gpx")
+    parse_file("data/new_river_50k.gpx", "path")
+
+    # Davidson Track 5K files
+    parse_file("data/dav_track_5k.tcx", "tcx")
+    parse_file("data/dav_track_5k.tcx", "path")
+    parse_file("data/dav_track_5k.gpx", "gpx")
+    parse_file("data/dav_track_5k.gpx", "path")
+
+    # activity_4564516081 files
+    parse_file("data/activity_4564516081.tcx", "tcx")
+    parse_file("data/activity_4564516081.tcx", "path")
+    parse_file("data/activity_4564516081.gpx", "gpx")
+    parse_file("data/activity_4564516081.gpx", "path")
+
+    # activity_4564516081 files
+    parse_file("data/activity_607442311.tcx", "tcx")
+    parse_file("data/activity_607442311.tcx", "path")
+    parse_file("data/activity_607442311.gpx", "gpx")
+    parse_file("data/activity_607442311.gpx", "path")
