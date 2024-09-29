7f5a48c481a456e5c2ae39c53890be6d3e65ed00
Chris Joakim <christopher.joakim@gmail.com>
2024-09-29 18:05:49 -0400

    path parser tests, all tests processing

--- tests/test_path_handler.py
+++ tests/test_path_handler.py
@@ -2,44 +2,45 @@ import json
 
 import ggps
 
+import pytest
+
 from tests.helpers.unit_test_helper import UnitTestHelper
 
 # pytest -v tests/test_path_handler.py
 
 
-def test_str():
-    handler = ggps.PathHandler()
-    actual = str(handler)
-    expected = "{}"
-    assert actual == expected
-
-    filename = "data/twin_cities_marathon.gpx"
+def test_twin_cities_marathon_gpx():
     expected_trackpoint_count = 2256
+    filename = "data/twin_cities_marathon.gpx"
     handler = ggps.PathHandler()
     handler.parse(filename)
-    obj = json.loads(str(handler))
-    assert obj["gpx|trk|trkseg|trkpt@lat"] == expected_trackpoint_count
 
     helper = UnitTestHelper(handler)
-    # helper.assert_str()
+    helper.assert_filename(filename)
+    helper.assert_ggps_version()
+    helper.assert_ggps_parsed_at()
 
-
-def test_counts():
-    filename = "data/twin_cities_marathon.gpx"
-    expected_trackpoint_count = 2256
-    handler = ggps.PathHandler()
-    handler.parse(filename)
-    counter = handler.path_counter
+    counter = handler.get_data()["path_counter"]
     assert counter["gpx|trk|trkseg|trkpt@lat"] == expected_trackpoint_count
     assert counter["gpx|metadata|time"] == 1
-    assert handler.curr_depth() == 0
+    helper.assert_str()
 
 
-def test_base_parse_hhmmss():
-    filename = "data/twin_cities_marathon.gpx"
+def test_twin_cities_marathon_tcx():
+    expected_trackpoint_count = 2256
+    filename = "data/twin_cities_marathon.tcx"
     handler = ggps.PathHandler()
     handler.parse(filename)
-    hhmmss = handler.parse_hhmmss("")
-    assert hhmmss == ""
-    hhmmss = handler.parse_hhmmss("xxx")
-    assert hhmmss == ""
+
+    helper = UnitTestHelper(handler)
+    helper.assert_filename(filename)
+    helper.assert_ggps_version()
+    helper.assert_ggps_parsed_at()
+
+    counter = handler.get_data()["path_counter"]
+    assert (
+        counter["TrainingCenterDatabase|Activities|Activity|Lap|Track|Trackpoint"]
+        == expected_trackpoint_count
+    )
+    assert counter["TrainingCenterDatabase|Author|Name"] == 1
+    helper.assert_str()
